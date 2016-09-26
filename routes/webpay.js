3d0fe6dc32357abfb81238ed171137446ffb058a
Jaime Vasquez <rodrigoxv@gmail.com>
2016-09-26 15:17:50 -0300

    url

--- routes/webpay.js
+++ routes/webpay.js
@@ -211,10 +211,10 @@ router.use('/init',function(req,res,next){
             signxml.setOpts("%commerceId%",597020000541);
             signxml.setOpts("%buyOrder%",orderid);
             signxml.setOpts("%sessionId%",sessionId);
-            signxml.setOpts("%returnURL%","http://200.120.84.207:3000/webpay/return");
-            signxml.setOpts("%finalURL%","http://200.120.84.207:3000/webpay/final");
-            //signxml.setOpts("%returnURL%","https://app-theprintlab.herokuapp.com/webpay/return");
-            //signxml.setOpts("%finalURL%","https://app-theprintlab.herokuapp.com/webpay/final");
+            //signxml.setOpts("%returnURL%","http://200.120.84.207:3000/webpay/return");
+            //signxml.setOpts("%finalURL%","http://200.120.84.207:3000/webpay/final");
+            signxml.setOpts("%returnURL%","https://app-theprintlab.herokuapp.com/webpay/return");
+            signxml.setOpts("%finalURL%","https://app-theprintlab.herokuapp.com/webpay/final");
             //
             signxml.setOpts("%amount%",total);
             signxml.setOpts("%commerceCode%","597020000541");
